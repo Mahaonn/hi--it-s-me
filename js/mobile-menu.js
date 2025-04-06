e27fd7b65a4c0ddaecd305d880a2659533dccae3
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2025-04-06 17:36:52 +0200

    Added functionality to prevent scrolling when modal is open
    Implemented .no-scroll class in CSS to block overflow. Updated JavaScript to add/remove .no-scroll class on <body>. Applied logic for scrollbar width compensation in modal open/close handlers.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,22 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const body = document.querySelector("body");
 
-const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+const toggleMenu = () => {
+  const scrollBarWidth =
+    window.innerWidth - document.documentElement.clientWidth;
+
+  if (mobileMenu.classList.contains("is-open")) {
+    mobileMenu.classList.remove("is-open");
+    body.classList.remove("no-scroll");
+    body.style.paddingRight = "";
+  } else {
+    mobileMenu.classList.add("is-open");
+    body.classList.add("no-scroll");
+    body.style.paddingRight = `${scrollBarWidth}px`;
+  }
+};
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
